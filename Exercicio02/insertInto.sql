f58331bc8a96b338c08cfe0c6df0a93552231166
Gustavo <jgustavocambui@gmail.com>
2022-06-23 15:50:34 -0300

    Adiciona exercícios

---
+++ Exercicio02/insertInto.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_produtos(nome, quantidade, categoria, valor) VALUES ("Café", 123, "Alimento", 9.75);
+INSERT INTO tb_produtos(nome, quantidade, categoria, valor) VALUES ("Arroz", 322, "Alimento", 6.99);
+INSERT INTO tb_produtos(nome, quantidade, categoria, valor) VALUES ("Óleo", 232, "Alimento", 8.75);
+INSERT INTO tb_produtos(nome, quantidade, categoria, valor) VALUES ("Açucar", 332, "Alimento", 5.66);
+INSERT INTO tb_produtos(nome, quantidade, categoria, valor) VALUES ("Cotonete", 52, "Higiene", 9.75);
+INSERT INTO tb_produtos(nome, quantidade, categoria, valor) VALUES ("Creme Dental", 126, "Higiene", 4.55);
+INSERT INTO tb_produtos(nome, quantidade, categoria, valor) VALUES ("Fio Dental", 112, "Higiene", 12.75);
+INSERT INTO tb_produtos(nome, quantidade, categoria, valor) VALUES ("Aerosol", 222, "Higiene", 9.75);
---
+++ Exercicio02/selectFrom&Update.sql
@@ -0,0 +1,7 @@
+SELECT * FROM tb_produtos WHERE valor > 500;
+
+SELECT * FROM tb_produtos WHERE valor < 500;
+
+UPDATE tb_produtos
+SET quantidade = 195, valor = 11.99
+WHERE id = 8;
